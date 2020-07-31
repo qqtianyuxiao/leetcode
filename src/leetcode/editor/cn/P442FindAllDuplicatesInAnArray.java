ab13d659b5e05f60db160a88e1ef6941de041fc4
tyx <914464897@qq.com>
2020-07-31 15:45:32 +0800

    leetcode442

---
+++ src/leetcode/editor/cn/P442FindAllDuplicatesInAnArray.java
@@ -0,0 +1,17 @@
+package leetcode.editor.cn;
+
+import java.util.List;
+
+//数组中重复的数据
+class FindAllDuplicatesInAnArray {
+    public static void main(String[] args) {
+        Solution solution = new FindAllDuplicatesInAnArray().new Solution();
+    }
+
+    class Solution {
+        public List<Integer> findDuplicates(int[] nums) {
+            return null;
+        }
+    }
+
+}
